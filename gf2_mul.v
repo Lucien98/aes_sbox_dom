279d8ca3777c16559cbf0cc141024e2780e89ac6
lucien <2172856294@qq.com>
2024-11-11 00:40:28 -0800

    small optimazation for area

--- gf2_mul.v
+++ gf2_mul.v
@@ -29,8 +29,8 @@ generate
         assign B[0] = BxDI[0];
         assign B[1] = BxDI[1];
 
-        assign Q_norm[0] = ((A[1] ^ A[0]) & (B[1] ^ B[0])) ^ (A[0] & B[0]);
-        assign Q_norm[1] = ((A[1] ^ A[0]) & (B[1] ^ B[0])) ^ (A[1] & B[1]);
+        assign Q_norm[0] = (A[0] & B[1]) ^ (A[1] & B[0]) ^ (A[1] & B[1]);//((A[1] ^ A[0]) & (B[1] ^ B[0])) ^ (A[0] & B[0]);
+        assign Q_norm[1] = (A[0] & B[1]) ^ (A[1] & B[0]) ^ (A[0] & B[0]);//((A[1] ^ A[0]) & (B[1] ^ B[0])) ^ (A[1] & B[1]);
         
         assign Q[0] = Q_norm[0];
         assign Q[1] = Q_norm[1];
@@ -57,19 +57,19 @@ generate
         assign B[0] = BxDI[0];
         
         // HI MUL GF2^4 multiplier
-        assign PH[0] = ((A[3] ^ A[2]) & (B[3] ^ B[2])) ^ (A[2] & B[2]);
-        assign PH[1] = ((A[3] ^ A[2]) & (B[3] ^ B[2])) ^ (A[3] & B[3]);
+        assign PH[0] = (A[2] & B[3]) ^ (A[3] & B[2]) ^ (A[3] & B[3]);//((A[3] ^ A[2]) & (B[3] ^ B[2])) ^ (A[2] & B[2]);
+        assign PH[1] = (A[2] & B[3]) ^ (A[3] & B[2]) ^ (A[2] & B[2]);//((A[3] ^ A[2]) & (B[3] ^ B[2])) ^ (A[3] & B[3]);
         
         // LO MUL GF2^4 multiplier
-        assign PL[0] = ((A[1] ^ A[0]) & (B[1] ^ B[0])) ^ (A[0] & B[0]);
-        assign PL[1] = ((A[1] ^ A[0]) & (B[1] ^ B[0])) ^ (A[1] & B[1]);
+        assign PL[0] = (A[0] & B[1]) ^ (A[1] & B[0]) ^ (A[1] & B[1]);//((A[1] ^ A[0]) & (B[1] ^ B[0])) ^ (A[0] & B[0]);
+        assign PL[1] = (A[0] & B[1]) ^ (A[1] & B[0]) ^ (A[0] & B[0]);//((A[1] ^ A[0]) & (B[1] ^ B[0])) ^ (A[1] & B[1]);
 
         // MUL and SQUARE SCALE
         assign AA = {A[3], A[2]} ^ {A[1], A[0]};
         assign BB = {B[3], B[2]} ^ {B[1], B[0]};
 
-        assign P[1] = ((AA[1] ^ AA[0]) & (BB[1] ^ BB[0])) ^ (AA[0] & BB[0]);
-        assign P[0] = ((AA[1] ^ AA[0]) & (BB[1] ^ BB[0])) ^ (AA[1] & BB[1]) ^ P[1];
+        assign P[1] = ((AA[1] ^ AA[0]) & (BB[1] ^ BB[0])) ^ (AA[0] & BB[0]); //(AA[0] & BB[1]) ^ (AA[1] & BB[0]) ^ (AA[1] & BB[1]);//
+        assign P[0] = (AA[0] & BB[0]) ^ (AA[1] & BB[1]);
 
         // Output assignment
         assign Q_norm = {(PH ^ P), (PL ^ P)};
